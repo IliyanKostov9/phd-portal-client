e5a1a07ffd75c929aa0534e9df948f96b17374e5
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-01-06 23:29:29 +0200

    feat: added func (not tested yet)

--- src/app/api/doctoralCenter/admin/authenthicated/route.js
+++ src/app/api/doctoralCenter/admin/authenthicated/route.js
@@ -39,3 +39,44 @@ export async function GET(request) {
     return NextResponse.json({ error: "Server error!" }, { status: 500 });
   }
 }
+
+export async function DELETE(request) {
+  try {
+    const reqHeaders = await headers();
+    const accessToken = reqHeaders.get("authorization");
+
+    const body = await request.json();
+    const cookie = reqHeaders.get("Cookie");
+
+    const res = await fetch(
+      `${process.env.BASE_URL}/doctoralcenter/authenticated/delete`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+          Cookie: cookie
+        },
+
+        body: JSON.stringify(body)
+      }
+    );
+    const data = await res.json();
+    console.log(`Fetching delete response: ${JSON.stringify(data)}`);
+
+    const response = NextResponse.json(data, {
+      status: res.status
+    });
+
+    if (response.status > 400 && response.status < 600) {
+      return NextResponse.json(
+        { error: data.data.message },
+        { status: response.status }
+      );
+    }
+
+    return response;
+  } catch (error) {
+    return NextResponse.json({ error: "Server error!" }, { status: 500 });
+  }
+}
